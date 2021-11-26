d42b4b1138807808fe7d97d0bf4068980dc01369
zhoufeng <zhf883680@gmail.com>
2021-11-26 15:02:22 +0800

    add

---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,20 @@
+module.exports = [
+  { text: '首页', link: '/' },
+  {
+    text: '判断推理', link: '/pages/ece5b5/', items: [
+      { text: '类比推理', link: '/pages/ece5b5/' },
+      { text: '论证逻辑', link: '/pages/b20e69/' },
+      { text: '分析推理', link: '/pages/bfcfd2/' },
+      { text: '形式逻辑', link: '/pages/255f4a/' },
+      { text: '图形推理', link: '/pages/456300/' },
+    ]
+  },
+  {
+    text: '言语理解', link: '/pages/84736f/', items: [
+      { text: '考点', link: '/pages/84736f/' },
+      { text: '阅读理解', link: '/pages/51dfee/' },
+      { text: '词句表达', link: '/pages/5e32a7/' },
+      { text: '逻辑填空', link: '/pages/a205c3/' },
+    ]
+  }
+]
